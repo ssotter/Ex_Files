f9d7912f1b1abf455c7450bcc6f6bb6a58410159
Sergio Llopart Sotter <sergio.sotter@network-rg.com.br>
2022-02-18 17:02:16 -0300

    Projeto de leitura e escrita de arquivo

---
+++ src/application/Program.java
@@ -0,0 +1,67 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<Product> list = new ArrayList<>();
+		
+		System.out.println("Digite o caminho do arquivo para leitura: ");
+		String sourceFileStr  = sc.nextLine();
+		
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
+		
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
+		
+		String targetFileStr = sourceFolderStr + "\\out\\summary.csv";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+			String itemCsv = br.readLine();
+			
+			while (itemCsv != null) {
+				
+				String[] fields = itemCsv.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+
+				list.add(new Product(name, price, quantity));
+
+				itemCsv = br.readLine();
+			}
+			
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+				for (Product item : list) {
+					bw.write(item.getNome() + "," + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+				
+				System.out.println(targetFileStr + "  -> CRIADO COM SUCESSO!");
+			}
+			catch (IOException e) {
+				System.out.println("Erro gravando o arquivo: "); e.getMessage();
+			}					
+		}
+		catch (IOException e) {
+			System.out.println("Erro lendo o arquivo: " + e.getMessage());
+		}
+		sc.close();
+	}
+}
